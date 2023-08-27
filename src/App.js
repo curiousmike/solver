6c9a7288a4a8809850ecd16a2fc4604b36d97f41
Michael Coustier <curiousmike@gmail.com>
2023-08-27 10:46:17 -0700

    sorta working

--- src/App.js
+++ src/App.js
@@ -1,108 +1,142 @@
-import './App.css';
+import "./App.css";
 import { fiveLetterWords } from "./fiveLetterWords.js";
-import { Container } from './styles';
+import { Container } from "./styles";
 
 // This "appHeight" is the "fix" for iOS safari representing vh differently based on whether their footer is visible.
 const appHeight = () => {
-  const doc = document.documentElement
-  const height =  window.innerHeight // - 5
-  doc.style.setProperty('--app-height', `${height}px`)
-}
-window.addEventListener('resize', appHeight)
+  const doc = document.documentElement;
+  const height = window.innerHeight; // - 5
+  doc.style.setProperty("--app-height", `${height}px`);
+};
+window.addEventListener("resize", appHeight);
 appHeight();
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                  'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                  'y', 'z']
-// const availableLetters = {
-//   'a' : true, 'b': true, 'c': true, 'd': true, 'e': true,
-//   'f': true, 'g': true, 'h': true, 'i': true, 'j': true, 'k': true,
-//   'l': true, 'm': true, 'n': true, 'o': true, 'p': true, 'q': true,
-//   'r': true, 's': true, 't':true, 'u': true, 'v': true, 'w': true,
-//   'x': true, 'y': true, 'z': true
-// }
-const unavailableLetters = ['l']
+
+const unavailableLetters = ["s"];
 
 const status = {
   Unknown: 0,
   Wrong: 1,
   WrongSpot: 2,
-  Correct: 3
-}
+  Correct: 3,
+};
 
-const currentWord = [ 
-  {letter: 'a', status: status.Unknown}, 
-  {letter: 'b', status: status.Unknown},
-  {letter: 'a', status: status.Unknown},
-  {letter: 'c', status: status.Unknown},
-  {letter: 'i', status: status.Unknown}
-]
-const isWord = (wordToCheck) => {
-  //return fiveLetterWords.includes(isWord)
-  for (let i = 0; i < fiveLetterWords.length; i++) {
-    const word = fiveLetterWords[i]
-    console.log('dictionary word = ', word)
-    if (word === wordToCheck) {
-       console.log('match = ', wordToCheck)
-    return true }
+const currentWord = [
+  { letter: "", status: status.Unknown },
+  { letter: "p", status: status.WrongSpot },
+  { letter: "a", status: status.Correct },
+  { letter: "c", status: status.Correct },
+  { letter: "e", status: status.Correct },
+];
+const buildLetterCounts = () => {
+  const letterCounts = {};
+  currentWord.forEach((letter) => {
+    const theLetter = letter.letter;
+    if (theLetter !== "") {
+      if (!letterCounts[theLetter]) {
+        letterCounts[theLetter] = {};
+        letterCounts[theLetter].count = 1;
+      } else {
+        letterCounts[theLetter].count++;
+      }
+    }
+  });
+  // console.log('letterConts = ', letterCounts)
+  return letterCounts;
+};
+
+const buildLetterWholeCounts = (word) => {
+  const letterCounts = {};
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (!letterCounts[letter]) {
+      letterCounts[letter] = {};
+      letterCounts[letter].count = 1;
+    } else {
+      letterCounts[letter].count++;
+    }
   }
-  return false
-}
+  // console.log('letterConts = ', letterCounts)
+  return letterCounts;
+};
+
 const solveWord = () => {
   let count = 0;
-  let ridiculous = 0
-  console.log('start ...')
-  fiveLetterWords.forEach ( (word) => {
+  // let ridiculous = 0;
+  let validWordList = [];
+  console.log("start ...");
+
+  fiveLetterWords.forEach((word) => {
     // const word = fiveLetterWords[0]
-    let isValidWord = true
-    let validationIssue = null
+    let isValidWord = true;
+    var validationIssue = null;
     for (let wordLetterIndex = 0; wordLetterIndex < 5; wordLetterIndex++) {
-      const letter = word[wordLetterIndex]
+      count++;
+      const letter = word[wordLetterIndex];
       if (currentWord[wordLetterIndex].status === status.Correct) {
-        // then current index = OK
         if (letter !== currentWord[wordLetterIndex].letter) {
-          validationIssue = `Can only be ${currentWord[wordLetterIndex].letter} `
-          isValidWord = false
-          break
+          validationIssue = `Can only be ${currentWord[wordLetterIndex].letter} in spot ${wordLetterIndex}`;
+          isValidWord = false;
+          break;
         }
-      } else
-      if (currentWord[wordLetterIndex].status === status.Unknown) {
-        // then current index = OK
-        for (let u = 0; 0 < unavailableLetters.length; u++) {
+      } else if (currentWord[wordLetterIndex].status === status.Unknown) {
+        for (let u = 0; u < unavailableLetters.length; u++) {
+          count++;
           if (unavailableLetters[u] === letter) {
-            isValidWord = false
-            validationIssue = `Letter ${unavailableLetters[u]} cannot be used`
-            break
+            isValidWord = false;
+            validationIssue = `Letter not in puzzle: ${unavailableLetters[u]}`;
+            break;
           }
         }
-      } 
-      else if (currentWord[wordLetterIndex].status === status.WrongSpot)
-      {
-        // we know this is a valid letter, but not in this spot.
-        isValidWord = false
-        validationIssue = `Letter ${letter} is in puzzle but not index ${wordLetterIndex}`
-        break
+      } else if (
+        currentWord[wordLetterIndex].status === status.WrongSpot &&
+        letter === currentWord[wordLetterIndex].letter
+      ) {
+        isValidWord = false;
+        validationIssue = `Wrong spot: ${letter} is in puzzle but not index ${wordLetterIndex}`;
+        break;
       }
-      if (isValidWord) {
-        console.log('valid word = ', word)
-      } else {
-        console.log('validation issue  =',validationIssue)
+    }
+    // final validation - verify every letter in currentWord shows up in the dictionary word
+    // ocher should not be valid for ocean
+    if (isValidWord) {
+      const currentWordLetterCounts = buildLetterCounts(currentWord);
+      const dictionaryWordLetterCounts = buildLetterWholeCounts(word);
+      for (const [key, value] of Object.entries(currentWordLetterCounts)) {
+        // console.log('key/value = ', key, value)
+        count++;
+        if (
+          !dictionaryWordLetterCounts[key] ||
+          dictionaryWordLetterCounts[key].count < value.count
+        ) {
+          // console.log('wrong')
+          isValidWord = false;
+          // validationIssue = `The dictionary word doesn't have enough letters: ${key}`;
+          break;
+        }
       }
-      console.log('letter = ', letter)
     }
-    count++
-    if(count > 10) {
-      console.log('count > 10')
-      return
+
+    if (isValidWord) {
+      console.log("Valid : ", word);
+      validWordList.push(word);
+    } else {
+      // console.log(`Invalid:  ${word} validation issue ${validationIssue}`)
     }
-   })
-}
-solveWord()
+    count++;
+    // if (count % 1000) {
+    //   console.log(`Count ${count}`);
+    // }
+  });
+  console.log("total iterations to find this list = ", count);
+  console.log("validWordList = ", validWordList);
+};
+solveWord();
 
 function App() {
   return (
     <Container>
-      Total 5 Letter Words {fiveLetterWords.length}<br></br>
+      Total 5 Letter Words {fiveLetterWords.length}
+      <br></br>
       First Word {fiveLetterWords[0]}
     </Container>
   );
--- src/fiveLetterWords.js
+++ src/fiveLetterWords.js
@@ -6,7 +6,7 @@ export const fiveLetterWords = [
 "aback",
 "abaft",
 "abaka",
-"abamp",
+"abamp", 
 "abase",
 "abash",
 "abate",

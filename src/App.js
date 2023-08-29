aca46d9cb2c2dbd81995a08c5a27ae49a7d683fc
Michael Coustier <curiousmike@gmail.com>
2023-08-28 18:13:41 -0700

    getting closer to having it workable

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   LetterContainer,
   FiveLetterContainer,
   LetterLabelContainer,
+  Button,
+  SolveButton,
 } from "./styles";
 import Keyboard from "./components/keyboard";
 
@@ -19,132 +21,135 @@ const appHeight = () => {
 window.addEventListener("resize", appHeight);
 appHeight();
 
-let unavailableLetters = ["s"];
-
+// solveWord();
 const status = {
   Unknown: 0,
   WrongSpot: 1,
   Correct: 2,
 };
-
-const currentWord = [
+const startingCurrentWord = [
   { letter: "", status: status.Unknown },
-  { letter: "p", status: status.WrongSpot },
-  { letter: "a", status: status.Correct },
-  { letter: "c", status: status.Correct },
-  { letter: "e", status: status.Correct },
+  { letter: "", status: status.Unknown },
+  { letter: "", status: status.Unknown },
+  { letter: "", status: status.Unknown },
+  { letter: "", status: status.Unknown },
 ];
-const buildLetterCounts = () => {
-  const letterCounts = {};
-  currentWord.forEach((letter) => {
-    const theLetter = letter.letter;
-    if (theLetter !== "") {
-      if (!letterCounts[theLetter]) {
-        letterCounts[theLetter] = {};
-        letterCounts[theLetter].count = 1;
-      } else {
-        letterCounts[theLetter].count++;
-      }
-    }
-  });
-  // console.log('letterConts = ', letterCounts)
-  return letterCounts;
-};
-
-const buildLetterWholeCounts = (word) => {
-  const letterCounts = {};
-  for (let i = 0; i < word.length; i++) {
-    const letter = word[i];
-    if (!letterCounts[letter]) {
-      letterCounts[letter] = {};
-      letterCounts[letter].count = 1;
-    } else {
-      letterCounts[letter].count++;
-    }
-  }
-  // console.log('letterConts = ', letterCounts)
-  return letterCounts;
-};
-
-const solveWord = () => {
-  let count = 0;
-  // let ridiculous = 0;
-  let validWordList = [];
-  console.log("start ...");
-
-  fiveLetterWords.forEach((word) => {
-    // const word = fiveLetterWords[0]
-    let isValidWord = true;
-    var validationIssue = null;
-    for (let wordLetterIndex = 0; wordLetterIndex < 5; wordLetterIndex++) {
-      count++;
-      const letter = word[wordLetterIndex];
-      if (currentWord[wordLetterIndex].status === status.Correct) {
-        if (letter !== currentWord[wordLetterIndex].letter) {
-          validationIssue = `Can only be ${currentWord[wordLetterIndex].letter} in spot ${wordLetterIndex}`;
-          isValidWord = false;
-          break;
-        }
-      } else if (currentWord[wordLetterIndex].status === status.Unknown) {
-        for (let u = 0; u < unavailableLetters.length; u++) {
-          count++;
-          if (unavailableLetters[u] === letter) {
-            isValidWord = false;
-            validationIssue = `Letter not in puzzle: ${unavailableLetters[u]}`;
-            break;
-          }
-        }
-      } else if (
-        currentWord[wordLetterIndex].status === status.WrongSpot &&
-        letter === currentWord[wordLetterIndex].letter
-      ) {
-        isValidWord = false;
-        validationIssue = `Wrong spot: ${letter} is in puzzle but not index ${wordLetterIndex}`;
-        break;
-      }
-    }
-    // final validation - verify every letter in currentWord shows up in the dictionary word
-    // ocher should not be valid for ocean
-    if (isValidWord) {
-      const currentWordLetterCounts = buildLetterCounts(currentWord);
-      const dictionaryWordLetterCounts = buildLetterWholeCounts(word);
-      for (const [key, value] of Object.entries(currentWordLetterCounts)) {
-        // console.log('key/value = ', key, value)
-        count++;
-        if (
-          !dictionaryWordLetterCounts[key] ||
-          dictionaryWordLetterCounts[key].count < value.count
-        ) {
-          // console.log('wrong')
-          isValidWord = false;
-          // validationIssue = `The dictionary word doesn't have enough letters: ${key}`;
-          break;
-        }
-      }
-    }
-
-    if (isValidWord) {
-      console.log("Valid : ", word);
-      validWordList.push(word);
-    } else {
-      // console.log(`Invalid:  ${word} validation issue ${validationIssue}`)
-    }
-    count++;
-    // if (count % 1000) {
-    //   console.log(`Count ${count}`);
-    // }
-  });
-  console.log("total iterations to find this list = ", count);
-  console.log("validWordList = ", validWordList);
-};
-solveWord();
 
 function App() {
   const [keyboardData, setKeyboardData] = useState({});
+  const [currentWord, setCurrentWord] = useState(startingCurrentWord);
+  const [unavailableLetters, setUnavailableLetters] = useState([]);
+
+  const buildLetterCounts = () => {
+    const letterCounts = {};
+    currentWord.forEach((letter) => {
+      const theLetter = letter.letter;
+      if (theLetter !== "") {
+        if (!letterCounts[theLetter]) {
+          letterCounts[theLetter] = {};
+          letterCounts[theLetter].count = 1;
+        } else {
+          letterCounts[theLetter].count++;
+        }
+      }
+    });
+    // console.log('letterConts = ', letterCounts)
+    return letterCounts;
+  };
+
+  const buildLetterWholeCounts = (word) => {
+    const letterCounts = {};
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i];
+      if (!letterCounts[letter]) {
+        letterCounts[letter] = {};
+        letterCounts[letter].count = 1;
+      } else {
+        letterCounts[letter].count++;
+      }
+    }
+    // console.log('letterConts = ', letterCounts)
+    return letterCounts;
+  };
+
+  const solveWord = () => {
+    let count = 0;
+    // let ridiculous = 0;
+    let validWordList = [];
+    console.log("start ...");
+
+    fiveLetterWords.forEach((word) => {
+      // const word = fiveLetterWords[0]
+      let isValidWord = true;
+      var validationIssue = null;
+      for (let wordLetterIndex = 0; wordLetterIndex < 5; wordLetterIndex++) {
+        count++;
+        const letter = word[wordLetterIndex];
+        if (currentWord[wordLetterIndex].status === status.Correct) {
+          if (letter !== currentWord[wordLetterIndex].letter) {
+            validationIssue = `Can only be ${currentWord[wordLetterIndex].letter} in spot ${wordLetterIndex}`;
+            isValidWord = false;
+            break;
+          }
+        } else if (
+          currentWord[wordLetterIndex].status === status.Unknown ||
+          currentWord[wordLetterIndex].letter === ""
+        ) {
+          for (let u = 0; u < unavailableLetters.length; u++) {
+            count++;
+            if (unavailableLetters[u] === letter) {
+              isValidWord = false;
+              validationIssue = `Letter not in puzzle: ${unavailableLetters[u]}`;
+              break;
+            }
+          }
+        } else if (
+          currentWord[wordLetterIndex].status === status.WrongSpot &&
+          letter === currentWord[wordLetterIndex].letter
+        ) {
+          isValidWord = false;
+          validationIssue = `Wrong spot: ${letter} is in puzzle but not index ${wordLetterIndex}`;
+          break;
+        }
+      }
+      // final validation - verify every letter in currentWord shows up in the dictionary word
+      // ocher should not be valid for ocean
+      if (isValidWord) {
+        const currentWordLetterCounts = buildLetterCounts(currentWord);
+        const dictionaryWordLetterCounts = buildLetterWholeCounts(word);
+        for (const [key, value] of Object.entries(currentWordLetterCounts)) {
+          // console.log('key/value = ', key, value)
+          count++;
+          if (
+            !dictionaryWordLetterCounts[key] ||
+            dictionaryWordLetterCounts[key].count < value.count
+          ) {
+            // console.log('wrong')
+            isValidWord = false;
+            // validationIssue = `The dictionary word doesn't have enough letters: ${key}`;
+            break;
+          }
+        }
+      }
+
+      if (isValidWord) {
+        console.log("Valid : ", word);
+        validWordList.push(word);
+      } else {
+        // console.log(`Invalid:  ${word} validation issue ${validationIssue}`)
+      }
+      count++;
+      // if (count % 1000) {
+      //   console.log(`Count ${count}`);
+      // }
+    });
+    console.log("total iterations to find this list = ", count);
+    console.log("validWordList = ", validWordList);
+  };
 
   const handleKey = (key) => {
     if (key === "GO") {
-      // solveWord()
+      solveWord();
     } else {
       const updatedKeyboardData = { ...keyboardData };
       if (updatedKeyboardData[key]) {
@@ -154,13 +159,14 @@ function App() {
       }
       console.log("updatedKeybarod ", updatedKeyboardData);
       setKeyboardData(updatedKeyboardData);
-      unavailableLetters = [];
+      let unavailable = [];
       for (const [key, value] of Object.entries(updatedKeyboardData)) {
         if (value === 1) {
-          unavailableLetters.push(key);
+          unavailable.push(key.toLowerCase());
         }
       }
-      console.log("unavail = ", unavailableLetters);
+      console.log("unavail = ", unavailable);
+      setUnavailableLetters(unavailable);
       console.log("key = ", key);
     }
   };
@@ -168,8 +174,29 @@ function App() {
   const updateKeyEntry = (e) => {
     const index = e.target.getAttribute("data-id");
     const letter = e.target.value;
-    currentWord[index].letter = letter;
-    currentWord[index].status = status.Unknown;
+    const updatedCurrentWord = [...currentWord];
+    updatedCurrentWord[index].letter = letter ? letter : "";
+    updatedCurrentWord[index].status =
+      letter !== "" ? status.Correct : status.Unknown;
+    setCurrentWord(updatedCurrentWord);
+    console.log("updated = ", updatedCurrentWord);
+  };
+
+  const updateKeyStatus = (e) => {
+    const index = e.target.getAttribute("data-id");
+    const updatedCurrentWord = [...currentWord];
+
+    if (updatedCurrentWord[index].status === status.WrongSpot) {
+      updatedCurrentWord[index].status = status.Correct;
+      console.log(`index ${index} is ${status.Correct}`);
+    } else if (
+      updatedCurrentWord[index].status === status.Correct ||
+      updatedCurrentWord[index].status === status.Unknown
+    ) {
+      updatedCurrentWord[index].status = status.WrongSpot;
+      console.log(`index ${index} is ${status.WrongSpot}`);
+    }
+    setCurrentWord(updatedCurrentWord);
   };
 
   console.log("keyboardData = ", keyboardData);
@@ -179,7 +206,6 @@ function App() {
       <br></br>
       First Word {fiveLetterWords[0]}
       <FiveLetterContainer>
-        <LetterLabelContainer for="letter0">First</LetterLabelContainer>
         <LetterContainer
           data-id="0"
           type="text"
@@ -187,7 +213,12 @@ function App() {
           name="letter0"
           onChange={updateKeyEntry}
         />
-        <LetterLabelContainer for="letter1">Second</LetterLabelContainer>
+        <Button
+          data-id="0"
+          buttonColor={currentWord[0].status}
+          onClick={updateKeyStatus}
+        />
+
         <LetterContainer
           type="text"
           id="letter1"
@@ -195,7 +226,12 @@ function App() {
           data-id="1"
           onChange={updateKeyEntry}
         />
-        <LetterLabelContainer for="letter2">Third </LetterLabelContainer>
+        <Button
+          data-id="1"
+          buttonColor={currentWord[1].status}
+          onClick={updateKeyStatus}
+        />
+
         <LetterContainer
           type="text"
           id="letter2"
@@ -203,7 +239,12 @@ function App() {
           data-id="2"
           onChange={updateKeyEntry}
         />
-        <LetterLabelContainer for="letter3">Fourth</LetterLabelContainer>
+        <Button
+          data-id="2"
+          buttonColor={currentWord[2].status}
+          onClick={updateKeyStatus}
+        />
+
         <LetterContainer
           type="text"
           id="letter3"
@@ -211,7 +252,12 @@ function App() {
           data-id="3"
           onChange={updateKeyEntry}
         />
-        <LetterLabelContainer for="letter4">Fifth Letter </LetterLabelContainer>
+        <Button
+          data-id="3"
+          buttonColor={currentWord[3].status}
+          onClick={updateKeyStatus}
+        />
+
         <LetterContainer
           type="text"
           id="letter4"
@@ -219,7 +265,13 @@ function App() {
           data-id="4"
           onChange={updateKeyEntry}
         />
+        <Button
+          data-id="4"
+          buttonColor={currentWord[4].status}
+          onClick={updateKeyStatus}
+        />
       </FiveLetterContainer>
+      <SolveButton onClick={solveWord}>Solve</SolveButton>
       <Keyboard
         keyboardData={keyboardData}
         handleKeyPress={(e) => handleKey(e)}
--- src/styles.js
+++ src/styles.js
@@ -15,12 +15,43 @@ export const Container = styled.div`
 export const FiveLetterContainer = styled.div`
   display: flex;
   flex-direction: row;
-  margin-top: 32px;
+  margin-top: 128px;
+  margin-bottom: 128px;
 `;
 export const LetterContainer = styled.input`
-  width: 32px;
+  width: 48px;
+  height: 48px;
+  font-size: 48px;
 `;
 export const LetterLabelContainer = styled.label`
   margin-right: 8px;
   margin-left: 8px;
+`;
+
+export const Button = styled.button`
+  color: white;
+  background-color: ${(props) =>
+    props.buttonColor === 2 ? "rgb(83, 141, 78)" : "rgb(181, 159, 59)"};
+
+  outline: ${(props) => (props.buttonColor === 1 ? "2px solid red" : "")};
+  outline: ${(props) => (props.buttonColor === 2 ? "2px solid #007AFF" : "")};
+  min-width: 8%;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 4px;
+`;
+
+export const SolveButton = styled.button`
+  color: white;
+  background-color: white;
+  color: black;
+  outline: "2px solid #007AFF";
+  min-width: 8%;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 4px;
 `;
